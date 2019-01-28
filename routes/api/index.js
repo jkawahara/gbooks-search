28199cc4850a82e16446a289d2228a2857c6da64
John Kawahara <kawaharajohn@gmail.com>
2019-01-27 20:15:31 -0800

    Add Controller routing

---
+++ routes/api/index.js
@@ -0,0 +1,8 @@
+// *** Include Modules: npm (express), /books
+const router = require("express").Router();
+const bookRoutes = require("./books");
+
+// Book routes
+router.use("/books", bookRoutes);
+
+module.exports = router;
